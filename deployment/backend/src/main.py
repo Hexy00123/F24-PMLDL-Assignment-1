08ecd2736076edb8acbe93dc63682948e76f51a7
Ruslan Izmailov <r.izmajlov2018@yandex.ru>
2024-09-29 23:03:32 +0300

    feat: model choice via docker-compose file

--- deployment/backend/src/main.py
+++ deployment/backend/src/main.py
@@ -1,15 +1,16 @@
-from typing import Annotated
-from fastapi import FastAPI, Body
-from pydantic import BaseModel
+import os
 import pickle
-from src.text_process import TextPreprocessor
+from typing import Annotated
+from pydantic import BaseModel
+from fastapi import FastAPI, Body
 from fastembed import TextEmbedding
+from src.text_process import TextPreprocessor
 
 
 app = FastAPI()
 
 preprocessor = TextPreprocessor()
-with open("/model/model_transformer.pkl", mode="rb") as file:
+with open(f"/model/{os.environ['MODEL_FILENAME']}", mode="rb") as file:
     model = pickle.load(file)
 
 if len(model.steps) != 2:
@@ -27,9 +28,7 @@ def predict(predict_body: Annotated[PredictBody, Body()]):
     cleaned_text = preprocessor.clean_text(predict_body.input)
 
     if len(model.steps) == 2:
-        predictions = model.predict_proba([preprocessor.preprocess_text(cleaned_text)])[
-            0
-        ]
+        predictions = model.predict_proba([preprocessor.preprocess_text(cleaned_text)])[0]
     else:
         embedding = next(embeddings.embed(documents=cleaned_text))
         predictions = model.predict_proba([embedding])[0]
@@ -40,3 +39,4 @@ def predict(predict_body: Annotated[PredictBody, Body()]):
     }
 
     return responce
+        
